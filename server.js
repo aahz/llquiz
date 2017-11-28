6e45bd32d308d983c5a63225840da66150b234be
Andrew Ozdion <a.ozdion@lingualeo.com>
2017-11-28 13:46:09 +0300

    Added bot token argument parsing.

--- server.js
+++ server.js
@@ -11,6 +11,12 @@ const argv = require('yargs')
         description: 'Set MongoDB connection URL',
         demand: true,
     })
+    .option('token', {
+        alias: 't',
+        type: 'string',
+        description: 'Set Slack Bot API token for new candidates notifications',
+        demand: true,
+    })
     .help()
     .argv;
 
