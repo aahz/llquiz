73dbe033ddd71393751fe747e7cf7d828dd80ffa
Andrew Ozdion <a.ozdion@lingualeo.com>
2017-12-01 04:53:32 +0300

    Increased time restriction up to 2 minutes per question.

--- constants/restrictions.js
+++ constants/restrictions.js
@@ -1,6 +1,6 @@
 module.exports = {
     MOCK_NUMBER: 10,
-    TIME_FOR_ONE_QUESTION: 1000 * 60 * 1.5, // 1 minute 30 seconds
+    TIME_FOR_ONE_QUESTION: 1000 * 60 * 2, // 2 minutes
     MAX_OPINION: 3,
     MAX_COMMENT: 1000,
 };
